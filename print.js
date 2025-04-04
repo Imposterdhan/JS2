de8c296af0781dc31dcab5389f1c2f30bf019c18
Dhananjay <116055091+Imposterdhan@users.noreply.github.com>
2025-04-04 23:44:44 +0530

    Add files via upload

---
+++ print.js
@@ -0,0 +1,37 @@
+// // <!-- 1.     document.write -->
+
+
+// let marriagedate="23-10-2003"
+// document.write(marriagedate)
+
+// document.write("<br>")
+// document.write("lorkjnojer hbg hr")  //no space 
+
+// document.writeln("hello herhhu")   //space yes
+
+// console.log("dhananjay")
+
+// // alert("battery 10%")
+
+// // confirm("are u 18+")
+
+// let name=prompt("dhanu")
+// console.log(name)
+// document.writeln(name)
+
+
+// let age=prompt("enter the age")
+// if(age>=18)
+// {
+//     document.writeln("come to the heaven 💀")
+// }
+// else{
+
+//     document.writeln("go to the hell  18")
+// }
+
+
+let no=Number(prompt("enter the no"))
+let no2=Number(prompt("enter the no"))
+ans=no+no
+document.writeln(ans)
